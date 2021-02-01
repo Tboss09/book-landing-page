44e7aea9295ea8b5d517414e9dbeeef65ba4e443
Akosile Tayo <akosilety9@gmail.com>
2021-02-01 05:04:44 +0100

    i Started book landing page master

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className>
+            <div className="header">
+                <div className="header__logo"></div>
+                <div className="header__nav"></div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
