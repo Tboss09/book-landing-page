93ee0ff8e8feafbf97be890418ae8e6ec4513ee7
Akosile Tayo <akosilety9@gmail.com>
2021-02-01 11:03:52 +0100

    Modifying the content of index.html

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,30 @@
-
+import TabletImage from '../images/illustration-hero.svg'
 const Main = () => {
     return (
-        <div>
+        <main className="main">
+            <header className="main__header">
+                <img className="main__header--img" src={TabletImage} alt="" />
+            </header>
+            {/* Main Section */}
 
-        </div>
+            <section className="main__section">
+                <div className="main__section--header title">
+                    <h1 className="title__header">A Simple Bookmark Manager </h1>
+                    <p className="title__text">A clean and simple interface to organize your favorite websites.
+                    Open a new browser tab and see your sites load instantly.
+                    Try it for free</p>
+
+                    <div className=" title__btn">
+                        <button className="title__btn--blue">Get it on Chrome</button>
+                        <button className="title__btn--white">Get it on FireFox</button>
+                    </div>
+
+                </div>
+
+                {/* {Chrome/FireFox} Button */}
+
+            </section>
+        </main>
     )
 }
 
