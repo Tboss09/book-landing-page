eddab6029e21f1c06bcbbf3b9c49c364ed7fddd8
Akosile Tayo <akosilety9@gmail.com>
2021-02-01 06:49:04 +0100

    Successfully installed node sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import logo from '../images/logo-bookmark.svg';
+import hamBurger from '../images/icon-hamburger.svg';
 
 const Header = () => {
     return (
         <header className>
             <div className="header">
-                <div className="header__logo"></div>
-                <div className="header__nav"></div>
+                <div className="header__logo">
+                    <a href="/">
+                    <img src={logo} alt="" />
+                    </a>
+                </div>
+
+                <div className="header__nav">
+                    <img src={hamBurger} alt="nav menu" />
+                </div>
             </div>
         </header>
     )
