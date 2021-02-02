89001dbb2b1a2d460e75536ed9cfcc951afbea5a
Akosile Tayo <akosilety9@gmail.com>
2021-02-02 06:15:55 +0100

    adding styles to the tabs/panels

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,4 +1,4 @@
-import TabletImage from '../images/illustration-hero.svg'
+import TabletImage from '../images/illustration-features-tab-1.svg'
 import TabletImage2 from '../images/illustration-features-tab-2.svg'
 import PeopleInTabletImage from '../images/illustration-features-tab-3.svg'
 
@@ -19,43 +19,53 @@ const Panel = () => {
 
 
                 {/* panel One */}
-                <div className="panel">
-                    <div className="panel__img">
-                        <img src={TabletImage} alt="" />
-                    </div>
-                    <h1 className="panel__header">Bookmark in one click</h1>
-                    <p>Organize your bookmarks however you like. Our simple drag-and-drop interface
+                <div className="panels">
+                    <div className="panel">
+                        <div className="panel__img">
+                            <img src={TabletImage} alt={`${TabletImage} img`} />
+                        </div>
+
+                        <div className="panel__text">
+                            <h1 className="panel__text--header">Bookmark in one click</h1>
+                            <p className="panel__text--desc">Organize your bookmarks however you like. Our simple drag-and-drop interface
                         gives you complete control over how you manage your favourite sites.</p>
-                    <a href="#" className="panel__link">More Info</a>
-                </div>
+                            <a href="#" className="panel__text--link">More Info</a>
+                        </div>
 
-                {/* panel Three */}
-                <div className="panel">
-                    <div className="panel__img">
-                        <img src={TabletImage2} alt="" />
                     </div>
-                    <div className="panel__text">
-                        <h1 className="panel__header">Intelligent search</h1>
-                        <p className="panel__description">Our powerful search feature will help you find saved sites in no time at all.
+
+                    {/* panel Three */}
+                    <div className="panel">
+                        <div className="panel__img">
+                            <img src={TabletImage2} alt="" />
+                        </div>
+
+                        <div className="panel__text">
+                            <h1 className="panel__text--header">Intelligent search</h1>
+                            <p className="panel__text--desc">Our powerful search feature will help you find saved sites in no time at all.
                             No need to trawl through all of your bookmarks.</p>
-                        <a href="#" className="panel__link">More Info</a>
+                            <a href="#" className="panel__text--link">More Info</a>
+                        </div>
+
                     </div>
 
-                </div>
 
+                    {/* panel Three */}
+                    <div className="panel">
+                        <div className="panel__img">
+                            <img src={PeopleInTabletImage} alt="" />
+                        </div>
 
-                {/* panel Three */}
-                <div className="panel">
-                    <div className="panel__img">
-                        <img src={PeopleInTabletImage} alt="" />
-                    </div>
-                    <h1 className="panel__header">Share your bookmarks</h1>
+                        <div className="panel__text">
 
-                    <p className="panel__text">
-                        Easily share your bookmarks and collections with others. Create a shareable
-                        link that you can send at the click of a button.
+                            <h1 className="panel__text--header">Share your bookmarks</h1>
+                            <p className="panel__text--desc">
+                                Easily share your bookmarks and collections with others. Create a shareable
+                                link that you can send at the click of a button.
                         </p>
-                    <a href="#" className="panel__link">More Info</a>
+                            <a href="#" className="panel__text--link">More Info</a>
+                        </div>
+                    </div>
                 </div>
             </div>
 
